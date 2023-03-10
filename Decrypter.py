7d53a9ff6ed9f0e8323d3af1258cfb11e07589f1
sebastian.betancourth <sebastian.betancourth@quipux.com>
2023-03-09 19:39:11 -0500

    docs:se agrega comentarios explicando cada bloque de código.

--- Decrypter.py
+++ Decrypter.py
@@ -3,6 +3,7 @@ import math
 from colored import fg, attr
 
 
+# Función que calcula el fitness score de cada quagrams
 def calcScore(text, quadgrams):
     """Calculates the fitness of the given text"""
     text = "".join(text)
@@ -15,23 +16,28 @@ def calcScore(text, quadgrams):
             score += floor
     return score
 
-
+# se crea diccionario con cada uno de los quagrams en inglés y su respectiva frecuencia
 quadgrams = {}
 for line in open("english_quadgrams.txt"):
     quad, num = line.split(" ")
     quadgrams[quad] = int(num)
 
+# se calcula la probabilidad logarítmica de cada quagrams
 n = sum(quadgrams.values())
 for quad in quadgrams.keys():
     quadgrams[quad] = math.log10(float(quadgrams[quad] / n))
 floor = math.log10(0.01 / n)
 
+# se le pide al usuario la frase que se desencriptara
 OriginalText = input("Enter crypted text:\n")
 text = OriginalText.upper()
 
+# se crea la lista con el alfabeto en inglés y la primera llave
 alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 bestKey = alphabet[:]
 
+# Se explora el texto para encontrar caracteres especiales, estos se extraen y
+# se guardan en una lista con su respectivo index
 specialChars = []
 count = 0
 for char in text:
@@ -40,24 +46,32 @@ for char in text:
         text = text.replace(char, "", 1)
         count += 1
 
+"""Se dan los valores iniciales, como es el puntaje de la oración inicial, 
+el puntaje válido y se le da un valor muy pequeño al valor final para hacer una 
+Validación adecuada con los puntajes obtenidos en cada iteración
+"""
 initialScore = calcScore(list(text), quadgrams)
 validScore = None
 finalScore = -99e9
 
+#valores iniciales del padre para el algoritmo genético
 random.shuffle(bestKey)
 parentKey, parentScore, parentGuess = bestKey[:], finalScore, []
 
 reset = fg(15) + attr(0)
 noChanges = 0
 currentGen = 0
+#Inicializamos el algoritmo genético
 while 1:
     currentGen += 1
+    # se obtiene el fitness score para la llave padre.
     for char in text:
         idx = alphabet.index(char)
         parentGuess.append(parentKey[idx])
     parentScore = calcScore(parentGuess, quadgrams)
 
     count = 0
+    # inicializamos el ciclo para calcular los valores del hijo.
     while count < 100:
         a = random.randint(0, len(alphabet) - 1)
         b = random.randint(0, len(alphabet) - 1)
@@ -65,11 +79,14 @@ while 1:
         childKey, childGuess = parentKey[:], []
         childKey[a], childKey[b] = childKey[b], childKey[a]
 
+        # se obtiene el fitness score para la llave hijo.
         for char in text:
             idx = alphabet.index(char)
             childGuess.append(childKey[idx])
         childScore = calcScore(childGuess, quadgrams)
 
+        """ Se evalúa si el puntaje del hijo es mayor al del padre en caso de ser 
+        así se establece el hijo como nuevo padre."""
         if childScore > parentScore:
             parentScore, parentKey, parentGuess = (
                 childScore,
@@ -79,6 +96,11 @@ while 1:
             count = 0
         count += 1
 
+
+    """ Se evalúa si el puntaje padre es mayor al valor final, si esto se cumple 
+    se establecen los valores del padre como los mejores valores.
+    """
+
     if parentScore > finalScore:
         bestGen, finalScore, bestKey, bestGuess = (
             currentGen,
@@ -87,6 +109,9 @@ while 1:
             parentGuess[:],
         )
 
+    """se evalúa si es puntaje final es igual al validScore en caso de que se cumpla
+    se acumula la iteración, ya que se define que cuando se cumpla esto 100 veces
+    el mensaje ya fue desencriptad y se rompe el ciclo."""
     if finalScore == validScore:
         noChanges += 1
         if noChanges == 100:
@@ -101,10 +126,12 @@ while 1:
     )
     print(f"Guessed text: {fg(243)}{attr(1)}{''.join(bestGuess)}{reset}")
 
+# se agregan los caracteres especiales que fueron retirados del mensaje.
 for char in specialChars:
     sC, idx = char
     bestGuess.insert(idx, sC)
 
+# se agregan las mayúsculas en las posiciones adecuadas.
 for i in range(len(OriginalText)):
     if not OriginalText[i].isupper():
         bestGuess[i] = bestGuess[i].lower()
